37ae65a7bf87120736f77ac9279025dd45f8a9bb
Titusz Pan <titusz.pan@gmail.com>
2023-02-28 22:17:26 +0100

    Add dummy pay task

---
+++ lnpayroll/tasks.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+def pay(pk):
+    # type: (int) -> Payment
+    """Execute Lighning payment."""
+
+    # Retrieve Payment object from database and validate status
+    # Decode
+    # Get Callback
+    # Validate LN SERVICE payment rules
+    # Get Exchange Rate
+    # Get Invoice
+    # Pay Invoice
