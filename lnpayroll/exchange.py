f8d38a78220aa9e7e2937bfdd69f3364a9a47dce
Titusz Pan <titusz.pan@gmail.com>
2023-02-28 22:13:18 +0100

    Add to_msats function

--- lnpayroll/exchange.py
+++ lnpayroll/exchange.py
@@ -6,55 +6,68 @@ from loguru import logger as log
 from constance import config
 import lnpayroll as lnp
 
-__all__ = ["get_fx_rate"]
+__all__ = [
+    "get_fx_rate",
+    "to_msats",
+]
 
 
 class CryptoCompare:
     url = "https://min-api.cryptocompare.com/data/price"
 
     @classmethod
-    def fx_rate(cls, src: str, dst: str) -> Decimal:
+    def fx_rate(cls, src: str, dst: str) -> lnp.FxRate:
         """Return exchange for source to destination currency"""
         params = dict(fsym=src, tsyms=dst)
         response = requests.get(cls.url, params=params)
         fx_rate = Decimal(response.json()[dst])
-        return fx_rate
+        fx_rate_obj = lnp.FxRate(src=src, rate=fx_rate, dst=dst, provider=cls.__name__)
+        return fx_rate_obj
 
 
 class ExchangeRateHost:
     url = "https://api.exchangerate.host/latest"
 
     @classmethod
-    def fx_rate(cls, src: str, dst: str) -> Decimal:
+    def fx_rate(cls, src: str, dst: str) -> lnp.FxRate:
         """Return exchange for source to destination currency"""
         params = dict(base=src, symbols=dst, source="crypto")
         result = requests.get(cls.url, params=params)
         fx_rate = Decimal(result.json()["rates"][dst])
-        return fx_rate
+        fx_rate_obj = lnp.FxRate(src=src, rate=fx_rate, dst=dst, provider=cls.__name__)
+        return fx_rate_obj
 
 
 providers = [CryptoCompare, ExchangeRateHost]
 
 
-def get_fx_rate(src: str, dst: str) -> Decimal:
+def get_fx_rate(src: str, dst: str) -> lnp.FxRate:
     """Get cached exchange rate"""
     key = f"{src}:{dst}"
     fx_rate = cache.get(key)
-    if fx_rate and fx_rate > 0:
+    if fx_rate:
         log.debug(f"Using cached exchangerate {fx_rate}")
         return fx_rate
 
     # Fetch exchange rate and update cache
     for provider in providers:
-        pname = provider.__name__
         try:
             fx_rate = provider.fx_rate(src, dst)
-            if not fx_rate > 0:
-                raise ValueError(f"Invalid exchange rate from {pname}: {fx_rate}")
             cache.set(key, fx_rate, timeout=config.FX_TIMEOUT)
-            log.debug(f"Updated exchange rate {key} to {fx_rate} via {pname}")
+            log.debug(
+                f"Updated exchange rate {key} to {fx_rate.rate} via {fx_rate.provider}"
+            )
             return fx_rate
         except Exception as e:
-            log.error(f"Failed to fetch exchange rate from {pname}: {e}")
+            log.error(f"Failed to fetch exchange rate from {provider.__name__}: {e}")
 
     raise lnp.ExchangeApiError("Could not aquire exchange rate from any provider!")
+
+
+def to_msats(amount, fx_rate):
+    # type: (lnp.Money, lnp.FxRate) -> int
+    """Convert any monetary amount to millisatoshis"""
+    amount_btc = amount.amount * fx_rate.rate
+    amount_sats = amount_btc * 100_000_000
+    amount_msats = amount_sats * 1000
+    return int(amount_msats)
