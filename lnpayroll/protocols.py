7ab838c39054aa03b29d9ae4670fa6b53f9cb389
Titusz Pan <titusz.pan@gmail.com>
2023-02-28 22:16:39 +0100

    Add Money protocol

--- lnpayroll/protocols.py
+++ lnpayroll/protocols.py
@@ -2,7 +2,10 @@
 from typing import Protocol
 from decimal import Decimal
 
-__all__ = ["FxProvider"]
+__all__ = [
+    "FxProvider",
+    "Money",
+]
 
 
 class FxProvider(Protocol):
@@ -10,3 +13,15 @@ class FxProvider(Protocol):
 
     def fx_rate(self, src: str, dst: str) -> Decimal:
         """Return exchange for source to destination currency"""
+
+
+class Money(Protocol):
+    """Monetary amount"""
+
+    @property
+    def amount(self) -> int:
+        pass
+
+    @property
+    def currency(self) -> str:
+        pass
